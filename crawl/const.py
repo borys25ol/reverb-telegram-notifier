022d8c82dff0202ba84af63c07c7d5fe5bf70310
BorysOl <oleynik.boris@gmail.com>
2023-05-24 23:24:46 +0300

    Add new scraping links

--- crawl/const.py
+++ crawl/const.py
@@ -4,6 +4,14 @@ LINKS_TO_CHECK = [
         "link": "https://reverb.com/p/mxr-m-291-dyna-comp-mini?ships_to=US_CON",
     },
     {
+        "name": "MXR Dyna Comp M102",
+        "link": "https://reverb.com/p/mxr-dyna-comp-m-102?ships_to=US_CON",
+    },
+    {
+        "name": "MXR Dyna Comp CSP102 Script Reissue",
+        "link": "https://reverb.com/p/mxr-csp102sl-dyna-comp-script-reissue?ships_to=US_CON",
+    },
+    {
         "name": "TC Electronic Hall Of Fame 2",
         "link": "https://reverb.com/p/tc-electronic-hall-of-fame-2?item_region=XX&ships_to=US_CON",
     },
