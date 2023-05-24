3b8261b580d6cdaca33abbc6115a3edeb12cd7ad
BorysOl <oleynik.boris@gmail.com>
2023-05-24 23:08:22 +0300

    Update scraping links

--- crawl/const.py
+++ crawl/const.py
@@ -1,19 +1,7 @@
 LINKS_TO_CHECK = [
     {
-        "name": "TC Electronic Flashback",
-        "link": "https://reverb.com/p/tc-electronic-flashback?ships_to=US_CON",
-    },
-    {
-        "name": "TC Electronic Flashback 2 Mini",
-        "link": "https://reverb.com/p/tc-electronic-flashback-ii-mini?ships_to=US_CON",
-    },
-    {
         "name": "MXR Dyna Comp Mini",
         "link": "https://reverb.com/p/mxr-m-291-dyna-comp-mini?ships_to=US_CON",
-    },
-    {
-        "name": "TC Electronic Flashback 2",
-        "link": "https://reverb.com/p/tc-electronic-flashback-ii-delay-and-looper?item_region=XX&ships_to=US_CON",
     },
     {
         "name": "TC Electronic Hall Of Fame 2",
