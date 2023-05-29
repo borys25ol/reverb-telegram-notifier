96f955297f595e544e393d4b255ca37bbfa387df
BorysOl <oleynik.boris@gmail.com>
2023-05-29 16:53:12 +0300

    Update scraping links

--- crawl/const.py
+++ crawl/const.py
@@ -1,17 +1,5 @@
 LINKS_TO_CHECK = [
     {
-        "name": "MXR Dyna Comp Mini",
-        "link": "https://reverb.com/p/mxr-m-291-dyna-comp-mini?ships_to=US_CON",
-    },
-    {
-        "name": "MXR Dyna Comp M102",
-        "link": "https://reverb.com/p/mxr-dyna-comp-m-102?ships_to=US_CON",
-    },
-    {
-        "name": "MXR Dyna Comp CSP102 Script Reissue",
-        "link": "https://reverb.com/p/mxr-csp102sl-dyna-comp-script-reissue?ships_to=US_CON",
-    },
-    {
         "name": "TC Electronic Hall Of Fame 2",
         "link": "https://reverb.com/p/tc-electronic-hall-of-fame-2?item_region=XX&ships_to=US_CON",
     },
@@ -19,4 +7,8 @@ LINKS_TO_CHECK = [
         "name": "Boss RT-20 Rotary",
         "link": "https://reverb.com/p/boss-rt-20-rotary-ensemble?item_region=XX&ships_to=US_CON",
     },
+    {
+        "name": "Dunlop QZone Cry Baby Fixed Wah",
+        "link": "https://reverb.com/p/dunlop-qz1-q-zone-cry-baby-fixed-wah?item_region=XX&ships_to=US_CON"
+    }
 ]
