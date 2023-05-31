323de6befa5935a7acb0ca3074341393196c4af7
BorysOl <oleynik.boris@gmail.com>
2023-05-31 17:31:20 +0300

    Update scraping links

--- crawl/const.py
+++ crawl/const.py
@@ -1,9 +1,5 @@
 LINKS_TO_CHECK = [
     {
-        "name": "TC Electronic Hall Of Fame 2",
-        "link": "https://reverb.com/p/tc-electronic-hall-of-fame-2?item_region=XX&ships_to=US_CON",
-    },
-    {
         "name": "Boss RT-20 Rotary",
         "link": "https://reverb.com/p/boss-rt-20-rotary-ensemble?item_region=XX&ships_to=US_CON",
     },
