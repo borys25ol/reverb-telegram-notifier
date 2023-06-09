01cd721e295edeb2aad454d7c548e71def5b4934
BorysOl <oleynik.boris@gmail.com>
2023-06-10 00:14:02 +0300

    Add filter for input scraping links

--- crawl/services/links.py
+++ crawl/services/links.py
@@ -3,4 +3,6 @@ from crawl.clients import get_mongo_scraping_link_collection
 
 def get_scraping_links() -> list[dict]:
     collection = get_mongo_scraping_link_collection()
-    return list(collection.find(filter={}, projection={"name": 1, "link": 1}))
+    return list(
+        collection.find(filter={"active": True}, projection={"name": 1, "link": 1})
+    )
